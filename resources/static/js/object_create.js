18d86b3b208d08e77c4381ff6d372d02841e99bf
SrikantKanekar <kanekarsrikant01@gmail.com>
2021-06-15 14:40:31 +0530

    removed drag and drop fallback

--- resources/static/js/object_create.js
+++ resources/static/js/object_create.js
@@ -1,15 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
-    const hasAdvancedUpload = (function () {
-        const div = document.createElement("div");
-        return (
-            ("draggable" in div || ("ondragstart" in div && "ondrop" in div)) &&
-            "FormData" in window &&
-            "FileReader" in window
-        );
-    })();
-
     const form = document.querySelector(".box");
     const input = form.querySelector('input[type="file"]');
     const filename = form.querySelector(".box_uploading span");
@@ -18,110 +9,82 @@ document.addEventListener('DOMContentLoaded', function () {
     const progressBar = document.querySelector(".progress-bar");
     let droppedFilename = false;
 
+    [
+        "drag",
+        "dragstart",
+        "dragend",
+        "dragover",
+        "dragenter",
+        "dragleave",
+        "drop",
+    ].forEach(function (event) {
+        form.addEventListener(event, function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    });
+
+    ["dragover", "dragenter"].forEach(function (event) {
+        form.addEventListener(event, function () {
+            form.classList.add("is_dragover");
+        });
+    });
+
+    ["dragleave", "dragend", "drop"].forEach(function (event) {
+        form.addEventListener(event, function () {
+            form.classList.remove("is_dragover");
+        });
+    });
+
+    form.addEventListener("drop", function (e) {
+        droppedFilename = e.dataTransfer.files[0].name;
+        updateFilename(droppedFilename);
+        submitForm();
+    });
+
     // automatically submit the form on file select
     input.addEventListener("change", function (e) {
         updateFilename(e.target.files[0].name);
         submitForm();
     });
 
-    // drag and drop files if the feature is available
-    if (hasAdvancedUpload) {
-        form.classList.add("has_advanced_upload");
-        [
-            "drag",
-            "dragstart",
-            "dragend",
-            "dragover",
-            "dragenter",
-            "dragleave",
-            "drop",
-        ].forEach(function (event) {
-            form.addEventListener(event, function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-            });
-        });
-
-        ["dragover", "dragenter"].forEach(function (event) {
-            form.addEventListener(event, function () {
-                form.classList.add("is_dragover");
-            });
-        });
-
-        ["dragleave", "dragend", "drop"].forEach(function (event) {
-            form.addEventListener(event, function () {
-                form.classList.remove("is_dragover");
-            });
-        });
-
-        form.addEventListener("drop", function (e) {
-            droppedFilename = e.dataTransfer.files[0].name;
-            updateFilename(droppedFilename);
-            submitForm();
-        });
-    }
-
-    // if the form was submitted
     form.addEventListener("submit", function (e) {
 
         // preventing the duplicate submissions if the current one is in progress
         if (form.classList.contains("is_uploading")) return false;
 
         uploadStart();
+        e.preventDefault();
+        const formData = new FormData(form);
 
-        // ajax file upload for modern browsers
-        if (hasAdvancedUpload) {
-            e.preventDefault();
-            const formData = new FormData(form);
-
-            if (droppedFilename) {
-                formData.append(input.getAttribute("name"), droppedFilename);
-            }
-
-            const request = new XMLHttpRequest();
-            request.open(
-                form.getAttribute("method"),
-                form.getAttribute("action"),
-                true
-            );
-            request.upload.addEventListener(
-                "progress",
-                function (e) {
-                    updateProgress(e);
-                },
-                false
-            );
-            request.onreadystatechange = function () {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
-                        let data = JSON.parse(request.responseText);
-                        handleSuccess(data);
-                    } else {
-                        handleError(request.responseText);
-                    }
-                }
-            };
-            request.send(formData);
-        } else {
-            // fallback Ajax solution upload for older browsers
-            let iframeName = "uploadiframe" + new Date().getTime()
-            let iframe = document.createElement("iframe");
-
-            iframe.setAttribute("name", iframeName);
-            iframe.style.display = "none";
-
-            document.body.appendChild(iframe);
-            form.setAttribute("target", iframeName);
-
-            iframe.addEventListener("load", function () {
-                let data = JSON.parse(iframe.contentDocument.body.innerHTML);
-                form.classList.remove("is_uploading");
-                form.classList.add(data === true ? "is_success" : "is_error");
-                form.removeAttribute("target");
-                if (!data) errorMsg.textContent = data;
-                iframe.parentNode.removeChild(iframe);
-            });
+        if (droppedFilename) {
+            formData.append(input.getAttribute("name"), droppedFilename);
         }
+
+        const request = new XMLHttpRequest();
+        request.open(
+            form.getAttribute("method"),
+            form.getAttribute("action"),
+            true
+        );
+        request.upload.addEventListener(
+            "progress",
+            function (e) {
+                updateProgress(e);
+            },
+            false
+        );
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    let data = JSON.parse(request.responseText);
+                    handleSuccess(data);
+                } else {
+                    handleError(request.responseText);
+                }
+            }
+        };
+        request.send(formData);
     });
 
     function updateFilename(name) {
@@ -134,6 +97,11 @@ document.addEventListener('DOMContentLoaded', function () {
         form.dispatchEvent(event);
     }
 
+    function uploadStart() {
+        form.classList.add("is_uploading");
+        form.classList.remove("is_error");
+    }
+
     function updateProgress(e) {
         if (e.lengthComputable) {
             const progress = (e.loaded / e.total) * 100;
@@ -141,6 +109,11 @@ document.addEventListener('DOMContentLoaded', function () {
             progressBar.setAttribute("style", "width:" + progress + "%");
             if (progress === 100) scanningStart();
         }
+    }
+
+    function scanningStart() {
+        form.classList.remove("is_uploading");
+        form.classList.add("is_scanning");
     }
 
     function handleSuccess(data) {
@@ -153,31 +126,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    function handleError(errorMessage) {
-        form.classList.remove("is_uploading");
-        form.classList.remove("is_scanning");
-        form.classList.add("is_error");
-        errorMsg.textContent = errorMessage;
-    }
-
-    function uploadStart() {
-        form.classList.add("is_uploading");
-        form.classList.remove("is_error");
-    }
-
-    function scanningStart() {
-        form.classList.remove("is_uploading");
-        form.classList.add("is_scanning");
-    }
-
     function creatingObject(data) {
         form.classList.remove("is_scanning");
         if (data.success === "true") {
             form.classList.add("is_success");
         } else {
             form.classList.add("is_error");
-            errorMsg.textContent = "error creating object";
+            errorMsg.textContent = "Error creating object";
         }
+    }
+
+    function handleError(errorMessage) {
+        form.classList.remove("is_uploading");
+        form.classList.remove("is_scanning");
+        form.classList.add("is_error");
+        errorMsg.textContent = errorMessage;
     }
 
     function clearPage() {
@@ -188,13 +151,5 @@ document.addEventListener('DOMContentLoaded', function () {
     restart.addEventListener("click", function (e) {
         e.preventDefault();
         form.classList.remove("is_error", "is_success");
-    });
-
-    // Firefox focus bug fix for file input
-    input.addEventListener("focus", function () {
-        input.classList.add("has_focus");
-    });
-    input.addEventListener("blur", function () {
-        input.classList.remove("has_focus");
     });
 });
