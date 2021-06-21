1c709d36dcfd8a63b9e954741357b4bf07944770
SrikantKanekar <kanekarsrikant01@gmail.com>
2021-06-21 13:19:41 +0530

    centered object and zoomed camera

--- resources/static/js/object_three.js
+++ resources/static/js/object_three.js
@@ -1,6 +1,7 @@
 // Canvas
 const canvasContainer = document.querySelector(".canvas_container");
 const canvas = document.querySelector('#canvas');
+let currentModel;
 
 const sizes = {
     width: canvasContainer.clientWidth,
@@ -16,12 +17,10 @@ scene.background = new THREE.Color(0xdddddd);
 // scene.add(axesHelper);
 
 // camera
-camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(2, 2, 2);
+camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 
 // OrbitControls
 let controls = new THREE.OrbitControls(camera, canvas);
-controls.update();
 controls.addEventListener('change', () => renderer.render(scene, camera));
 
 // renderer
@@ -36,18 +35,22 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
 
-// lightning
-const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
-scene.add(hemiLight);
+initLighting();
 
-spotLight = new THREE.SpotLight(0xffa95c, 4);
-spotLight.position.set(-50, 50, 50);
-spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
-spotLight.shadow.mapSize.width = 1024 * 4;
-spotLight.shadow.mapSize.height = 1024 * 4;
-scene.add(spotLight);
+function initLighting() {
+    const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+    scene.add(hemiLight);
 
+    const spotLight = new THREE.SpotLight(0xffa95c, 4);
+    spotLight.position.set(-50, 50, 50);
+    spotLight.castShadow = true;
+    spotLight.shadow.bias = -0.0001;
+    spotLight.shadow.mapSize.width = 1024 * 4;
+    spotLight.shadow.mapSize.height = 1024 * 4;
+    scene.add(spotLight);
+}
+
+// loading manager
 const manager = new THREE.LoadingManager();
 manager.onLoad = function () {
     $('#canvasLoader').delay(100).fadeOut('slow', function () {
@@ -55,64 +58,78 @@ manager.onLoad = function () {
     });
 }
 manager.onError = function (url) {
-    const errorContainer = document.querySelector('.canvas_error_container');
-    errorContainer.style.display = "flex";
-    errorContainer.firstElementChild.textContent = "Error loading " + url;
+    showCanvasError("Error loading " + url)
 }
 
-let currentModel;
+// material for obj and stl files
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff,
+    metalness: .25,
+    roughness: 0.1,
+    transparent: true,
+    transmission: 1.0,
+    side: THREE.DoubleSide,
+    clearcoat: 1.0,
+    clearcoatRoughness: .25
+});
 
 function showModel(url, extension, error, sizeError) {
-    updateCanvas();
-
+    updateCanvasDimension();
     if (extension === "glb") {
         new THREE.GLTFLoader(manager).load(url, function (gltf) {
-            currentModel = gltf.scene;
-            gltf.scene.children[0].traverse(n => {
-                if (n.isMesh) {
-                    n.castShadow = true;
-                    n.receiveShadow = true;
-                    if (n.material.map) n.material.map.anisotropy = 1;
-                }
-            });
-            scene.add(gltf.scene);
-            renderer.render(scene, camera);
-            const verified = verifyModel();
-            sizeError(verified);
+            setObject(gltf.scene)
+            sizeError(verifyModel());
         }, undefined, function (e) {
             error(e);
-            const errorContainer = document.querySelector('.canvas_error_container');
-            errorContainer.style.display = "flex";
-            errorContainer.firstElementChild.textContent = e.message;
+            showCanvasError(e.message)
         });
     } else if (extension === "obj") {
         new THREE.OBJLoader(manager).load(url, function (object) {
-                currentModel = object;
-                scene.add(object);
-            }, undefined, function (error) {
-                console.log(error);
+                object.traverse(function (child) {
+                    if (child instanceof THREE.Mesh) child.material = material
+                });
+                setObject(object)
+                sizeError(verifyModel());
+            }, undefined, function (e) {
+                error(e);
+                showCanvasError(e.message)
             }
         );
     } else if (extension === "STL") {
         new THREE.STLLoader(manager).load(url, function (geometry) {
-                const material = new THREE.MeshPhysicalMaterial({
-                    color: 0xb2ffc8,
-                    metalness: .25,
-                    roughness: 0.1,
-                    transparent: true,
-                    transmission: 1.0,
-                    side: THREE.DoubleSide,
-                    clearcoat: 1.0,
-                    clearcoatRoughness: .25
-                });
                 const mesh = new THREE.Mesh(geometry, material)
-                currentModel = mesh;
-                scene.add(mesh)
-            }, undefined, (error) => {
-                console.log(error);
+                setObject(mesh);
+                sizeError(verifyModel());
+            }, undefined, (e) => {
+                error(e);
+                showCanvasError(e.message)
             }
         );
     }
+}
+
+function setObject(object) {
+    currentModel = object;
+    const box = new THREE.Box3().setFromObject(object);
+
+    // zoom camera according to object size
+    const cameraPosition = Math.max(
+        box.max.y - box.min.y,
+        box.max.z - box.min.z,
+        box.max.x - box.min.x
+    ) * 0.5;
+    camera.position.x = cameraPosition;
+    camera.position.y = cameraPosition * 0.5;
+    camera.position.z = cameraPosition * 2.5;
+    camera.lookAt(0, 0, 0)
+
+    //position object to origin
+    object.position.x = -box.min.x - (box.max.x - box.min.x) / 2;
+    object.position.y = -box.min.y - (box.max.y - box.min.y) / 2;
+    object.position.z = -box.min.z - (box.max.z - box.min.z) / 2;
+
+    scene.add(object);
+    renderer.render(scene, camera);
 }
 
 function removeModel() {
@@ -120,48 +137,73 @@ function removeModel() {
     renderer.render(scene, camera);
 }
 
-// browser resize
+function showCanvasError(error) {
+    const errorContainer = document.querySelector('.canvas_error_container');
+    errorContainer.style.display = "flex";
+    errorContainer.firstElementChild.textContent = error;
+}
+
 window.addEventListener('resize', () => {
-    updateCanvas();
+    updateCanvasDimension();
 });
 
-function updateCanvas() {
-    // Update sizes
+function updateCanvasDimension() {
     sizes.width = canvasContainer.clientWidth;
     sizes.height = canvasContainer.clientHeight;
 
-    // Update camera
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
 
-    // Update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    renderer.render(scene, camera)
+    render();
 }
 
-// render
 function render() {
     renderer.render(scene, camera);
     controls.update();
 }
 
-// Gui Controller
-const gui = new dat.GUI({autoPlace: false});
-gui.close();
-gui.domElement.id = "gui";
-canvasContainer.appendChild(gui.domElement);
+initGui();
 
-const cameraFolder = gui.addFolder("Camera")
-cameraFolder.add(camera.position, "x", 0, 10, 0.01).onChange(render).listen()
-cameraFolder.add(camera.position, "y", 0, 10, 0.01).onChange(render).listen()
-cameraFolder.add(camera.position, "z", 0, 10, 0.01).onChange(render).listen()
+function initGui() {
+    const gui = new dat.GUI({autoPlace: false});
+    gui.close();
+    gui.domElement.id = "gui";
+    canvasContainer.appendChild(gui.domElement);
 
+    const data = {
+        sceneColor: scene.background.getHex(),
+        materialColor: material.color.getHex()
+    };
 
+    const cameraFolder = gui.addFolder("Camera")
+    cameraFolder.add(camera.position, "x", 0, 10, 0.01).onChange(render).listen()
+    cameraFolder.add(camera.position, "y", 0, 10, 0.01).onChange(render).listen()
+    cameraFolder.add(camera.position, "z", 0, 10, 0.01).onChange(render).listen()
+
+    const colorFolder = gui.addFolder("Colors")
+    colorFolder.addColor(data, "sceneColor").onChange(() => {
+        scene.background.setHex(Number(data.sceneColor.toString().replace('#', '0x')))
+        render()
+    });
+    colorFolder.addColor(data, "materialColor").onChange(() => {
+        material.color.setHex(Number(data.materialColor.toString().replace('#', '0x')))
+        render()
+    });
+}
+
+// verify if the model dimensions can be printed our printer
 function verifyModel() {
+    // TODO()
     return true;
 }
 
+function takeSnapshot() {
+    return canvas.toDataURL("image/png");
+}
+
+// not working for some reason
 // function takeSnapshot(q) {
 //     const temporaryCanvas = document.createElement('canvas');
 //     let context = temporaryCanvas.getContext('2d');
@@ -192,7 +234,3 @@ function verifyModel() {
 //     }
 //     return temporaryCanvas.toDataURL("image/png");
 // }
-
-function takeSnapshot() {
-    return canvas.toDataURL("image/png");
-}
